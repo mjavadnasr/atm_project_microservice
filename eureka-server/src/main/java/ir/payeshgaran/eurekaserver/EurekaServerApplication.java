657f6621178faec2a52bb307058014e024844af2
mnasrollah <m.javad.nasrolla@gmail.com>
2022-09-12 09:41:18 +0430

    clean code for eureka module

--- eureka-server/src/main/java/ir/payeshgaran/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ir/payeshgaran/eurekaserver/EurekaServerApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 @OpenAPIDefinition(info = @Info(title = "Eureka Module"))
-
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
